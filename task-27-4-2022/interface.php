d7c193145db71e9455c71d5c992544ea5c1f3f6f
Sujood-Mohammad <99989053+Sujood-Mohammad@users.noreply.github.com>
2022-04-28 16:59:52 +0300

    main

--- task-27-4-2022/interface.php
+++ task-27-4-2022/interface.php
@@ -32,3 +32,14 @@ interface Animal {
   foreach($animals as $animal) {
     $animal->makeSound();
   }
+
+
+
+
+
+//   Cat, Dog and Mouse are all classes that implement the Animal interface, which means that all of them are able
+//    to make a sound using the makeSound() method. Because of this, we can loop through all of the animals and tell them to make 
+//    a sound even if we don't know what type of animal each one is.
+
+// Since the interface does not tell the classes how to implement the method, each animal can make a sound in its own way.
+
---
+++ task-28-4-2022/task1.php
@@ -0,0 +1,9 @@
+<?php
+
+$em = $_POST['email'];
+echo $em;
+echo "<br>";
+$pass = $_POST['password'];
+echo $pass;
+
+?>
